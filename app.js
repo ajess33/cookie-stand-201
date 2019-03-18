24981ad8816c896436db753564a5b59104941965
Austin Jess <ajess33@gmail.com>
2019-03-18 17:30:01 -0500

    rendered allStores array to populate DOM

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-// First create an object literal that has the following:
-
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties
-// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-// Store the results for each location in a separate array... perhaps as a property of the object representing that location
-// Display the values of each array as unordered lists in the browser
-// Calculating the sum of these hourly totals; your output for each location should look like this:
-// 1st and Pike
-
-// 6am: 16 cookies
-// 7am: 20 cookies
-// 8am: 35 cookies
-// 9am: 48 cookies
-// 10am: 56 cookies
-// 11am: 77 cookies
-// 12pm: 93 cookies
-// 1pm: 144 cookies
-// 2pm: 119 cookies
-// 3pm: 84 cookies
-// 4pm: 61 cookies
-// 5pm: 23 cookies
-// 6pm: 42 cookies
-// 7pm: 57 cookies
-// 8pm: 29 cookies
-// Total: 657 cookies
-
-// Display lists on sales.html
-
 var hours = [
   '6am',
   '7am',
@@ -97,8 +68,6 @@ var pikeStore = {
   }
 };
 
-pikeStore.render();
-
 var seatacStore = {
   locationName: 'SeaTac Airport',
   minCustPerHour: 3,
@@ -150,8 +119,6 @@ var seatacStore = {
     document.body.appendChild(unorderedList);
   }
 };
-
-seatacStore.render();
 
 var seattleStore = {
   locationName: 'Seattle Center',
@@ -205,8 +172,6 @@ var seattleStore = {
   }
 };
 
-seattleStore.render();
-
 var capitolStore = {
   locationName: 'Capitol Hill',
   minCustPerHour: 20,
@@ -258,8 +223,6 @@ var capitolStore = {
     document.body.appendChild(unorderedList);
   }
 };
-
-capitolStore.render();
 
 var alkiStore = {
   locationName: 'Alki',
@@ -313,4 +276,8 @@ var alkiStore = {
   }
 };
 
-alkiStore.render();
+var allStores = [pikeStore, seatacStore, seattleStore, capitolStore, alkiStore];
+
+for (var i = 0; i < allStores.length; i++) {
+  allStores[i].render();
+}
